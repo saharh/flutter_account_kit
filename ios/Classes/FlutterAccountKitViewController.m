e46a2a6cb20957714e27e305f10198147b949100
Sahar Haddad <saharh09@gmail.com>
2019-08-04 13:19:55 +0300

    update accountkit and fix error

--- ios/Classes/FlutterAccountKitViewController.m
+++ ios/Classes/FlutterAccountKitViewController.m
@@ -52,7 +52,8 @@
     uiManager.secondLine = self.secondLine;
     viewController.uiManager = uiManager;
     viewController.enableSendToFacebook = true;
-    viewController.enableGetACall = true;
+    viewController.enableSMS = true;
+//    viewController.enableGetACall = true;
     
 //    viewController.uiManager = [[AKFSkinManager alloc] initWithSkinType:AKFSkinTypeContemporary primaryColor:[UIColor colorWithRed:0.28 green:0.62 blue:0.33 alpha:1.0]];
 }
